0eaf877c420025a5ef8fa40caa8c1c80f1c05c9d
Ramdan Ariadi <ramadanariadi@gmail.com>
2026-05-16 03:33:54 +0800

    refactor: remove unused @Autowired imports from ExportRequestService and DownloadService

--- src/main/java/id/grocery/tunas/download/DownloadService.java
+++ src/main/java/id/grocery/tunas/download/DownloadService.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import id.grocery.tunas.download.dto.DownloadDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
--- src/main/java/id/grocery/tunas/export/ExportRequestService.java
+++ src/main/java/id/grocery/tunas/export/ExportRequestService.java
@@ -15,7 +15,6 @@ import id.grocery.tunas.utils.ExportUtil;
 import io.vertx.core.json.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
